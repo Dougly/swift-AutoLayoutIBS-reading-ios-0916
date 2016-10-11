4315424387285c99214d70f11d83198641656490
Dougly <dgalante11@gmail.com>
2016-10-11 17:53:15 -0400

    finished reading and built demo

---
+++ AutoLayoutFun/AutoLayoutFun/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  AutoLayoutFun
+//
+//  Created by Douglas Galante on 10/11/16.
+//  Copyright © 2016 Douglas Galante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let blueView = UIView(frame: CGRect(x: 20.0, y: 20.0, width: 100.0, height: 100.0))
+//        blueView.backgroundColor = .blue
+//        view.addSubview(blueView)
+//        
+//        let redView = UIView(frame: CGRect(x: 150.0, y: 150.0, width: 40.0, height: 40.0))
+//        redView.backgroundColor = .red
+//        view.addSubview(redView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
